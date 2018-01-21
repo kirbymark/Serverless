f1bd45ce32f55e971f6ead71732df1117d4cce76
Mark J Kirby <mark@kirbyware.com>
2018-01-21 15:02:06 -0500

    attempt 2

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
-import webapp2
-import urllib
+# Copyright 2015 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-class ListAuthors(webapp2.RequestHandler):
-  def get(self):
-    self.response.headers["Content-Type"] = "text/plain"
-    self.response.write("List authors...")
+# [START app]
+import logging
 
-class ListEbooksByAuthor(webapp2.RequestHandler):
-  def get(self, author):
-    self.response.headers["Content-Type"] = "text/plain"
-    self.response.write('Show ebooks by "%s"...' % (urllib.unquote_plus(author)))
+from flask import Flask
 
-app = webapp2.WSGIApplication([
-  ("/", ListAuthors),
-  ("/(.*)", ListEbooksByAuthor),
-], debug=True)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
+if __name__ == '__main__':
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Google App Engine. See entrypoint in app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END app]
