f2f4b8f87e4443693c851fc7e5af82058867ff72
Mark J Kirby <mark@kirbyware.com>
2018-02-02 13:42:27 -0500

    testing

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World! This is just a short test'
+    return 'Hello World! This is just a short test and its running fine'
 
 
 @app.errorhandler(500)
